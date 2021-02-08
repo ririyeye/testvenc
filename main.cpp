f65c33fc933d98546938f648e4c61e51b1d5b389
ririyeye <200610237@qq.com>
2021-02-08 14:08:19 +0800

    add sample common

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+#ifdef __cplusplus
+#if __cplusplus
+extern "C" {
+#endif
+#endif /* End of #ifdef __cplusplus */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,22 +16,100 @@
 #include <sys/ioctl.h>
 
 #include "hi_common.h"
-//#include "sample_vio.h"
+#include "sample_vio.h"
 #include "mpi_sys.h"
-int main(int argc, char **argv)
+/******************************************************************************
+* function : show usage
+******************************************************************************/
+void SAMPLE_VIO_Usage(char *sPrgNm)
 {
-	HI_S32 s32Ret = HI_FAILURE;
-	HI_S32 s32Index;
-	HI_U32 u32VoIntfType = 0;
-	HI_U32 u32ChipId;
-	HI_MPI_SYS_GetChipId(&u32ChipId);
+    printf("Usage : %s <index>\n", sPrgNm);
+    printf("index:\n");
+    // printf("\t 0)VI (Online) - VPSS(Online) - VO.\n");
+    printf("\t 0)HT_VIO_LT8619C.\n");
+    printf("\t 1)WDR(Offline)- VPSS(Offline) - VO. LDC+DIS+SPREAD.\n");
+    printf("\t 2)Resolute Ratio Switch.\n");
+    printf("\t 3)GDC - VPSS LowDelay.\n");
+    printf("\t 4)Double WDR Pipe.\n");
+    printf("\t 5)FPN Calibrate & Correction.\n");
+    printf("\t 6)WDR Switch.\n");
+    printf("\t 7)90/180/270/0/free Rotate.\n");
+    printf("\t 8)UserPic.\n");
+    printf("\t 9)VI-VPSS-VO(MIPI_TX).\n\n");
 
-	if (HI3516C_V500 == u32ChipId) {
-		u32VoIntfType = 1;
-	} else {
-		u32VoIntfType = 0;
-	}
-
-	printf("123\n");
-	return 0;
+    printf("\t Hi3516DV300/Hi3559V200/Hi3556V200) vo HDMI output.\n");
+    printf("\t Hi3516CV500) vo BT1120 output.\n");
+    printf("\t If you have any questions, please look at readme.txt!\n");
+    return;
 }
+
+/******************************************************************************
+* function    : main()
+* Description : main
+******************************************************************************/
+#ifdef __HuaweiLite__
+int app_main(int argc, char *argv[])
+#else
+int main(int argc, char *argv[])
+#endif
+{
+    HI_S32 s32Ret = HI_FAILURE;
+    HI_S32 s32Index;
+    HI_U32 u32VoIntfType = 0;
+    HI_U32  u32ChipId;
+
+    if (argc < 2 || argc > 2)
+    {
+        SAMPLE_VIO_Usage(argv[0]);
+        return HI_FAILURE;
+    }
+
+    if (!strncmp(argv[1], "-h", 2))
+    {
+        SAMPLE_VIO_Usage(argv[0]);
+        return HI_SUCCESS;
+    }
+
+#ifdef __HuaweiLite__
+#else
+    signal(SIGINT, SAMPLE_VIO_HandleSig);
+    signal(SIGTERM, SAMPLE_VIO_HandleSig);
+#endif
+
+    HI_MPI_SYS_GetChipId(&u32ChipId);
+
+    if (HI3516C_V500 == u32ChipId)
+    {
+        u32VoIntfType = 1;
+    }
+    else
+    {
+        u32VoIntfType = 0;
+    }
+
+    SAMPLE_VIO_MsgInit();
+
+    s32Index = atoi(argv[1]);
+
+    s32Ret = HT_VIO_LT8619C(u32VoIntfType);
+
+
+    if (HI_SUCCESS == s32Ret)
+    {
+        SAMPLE_PRT("sample_vio exit success!\n");
+    }
+    else
+    {
+        SAMPLE_PRT("sample_vio exit abnormally!\n");
+    }
+
+    SAMPLE_VIO_MsgExit();
+
+    return s32Ret;
+}
+
+#ifdef __cplusplus
+#if __cplusplus
+}
+#endif
+#endif /* End of #ifdef __cplusplus */
